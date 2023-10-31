b1cfadeb065bd9df82212fa08d2cb995367f0dd9
yacine-yac <lentente1958@gmail.com>
2023-10-31 15:53:24 +0100

    turn product array not empty in orders

--- lib/db/schema/orders/index.ts
+++ lib/db/schema/orders/index.ts
@@ -4,7 +4,7 @@ import { Schema} from "mongoose";
 
 const mongoose=require('mongoose');
 const order:Schema<Iorder>=new mongoose.Schema({
-    number:{type:Number,index:true,unique:true},
+    number:{type:Number,unique:true},
     client:{type:Number,required:true},
     products:{type:[
                     {
@@ -12,7 +12,11 @@ const order:Schema<Iorder>=new mongoose.Schema({
                         product:{type:Number,required:true}
                     }
                    ],
-                   required:true
+                   required:true,
+                   validate:{
+                       validator:function(arr:[]){ return arr.length>0 && arr },
+                       message:'products must not be empty'
+                   }
             },
     total:{type:Number,required:true},
     deliveryPrice:{type:Number,required:true},
