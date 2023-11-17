9ed7a49b211d3ad2507b419a8ed6e8412ffe8997
yacine-yac <lentente1958@gmail.com>
2023-11-17 14:58:41 +0100

    create reusibale templates

---
+++ src/templates/hearderSection.tsx
@@ -0,0 +1,8 @@
+import "./style/headerSection.css";
+export default function HeaderSection(){
+    return <>
+       <section id="header-2" >
+                <button className="btn-side-list"></button>
+        </section>
+    </>
+}
