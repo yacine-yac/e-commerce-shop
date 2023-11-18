059a4ee62e4e2e16843f5e7c9bdcccbc5fa1d598
yacine-yac <lentente1958@gmail.com>
2023-11-18 17:28:23 +0100

    some header style changes

--- src/templates/hearderSection.tsx
+++ src/templates/hearderSection.tsx
@@ -1,8 +1,14 @@
+import Link from "next/link"
 import "./style/headerSection.css";
 export default function HeaderSection(){
     return <>
-       <section id="header-2" >
+        <header className="header-shadow">
+            <section className="up" id="header-1">
+                <span><Link href="/"><img src='./icons/logo.jpg' /></Link></span> 
+            </section>
+            <section className="down" id="header-2" >
                 <button className="btn-side-list"></button>
-        </section>
+            </section>
+        </header> 
     </>
 }
