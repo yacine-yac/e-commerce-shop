0b3aafedb12482286ea6a06c0921310961d697ca
yacine-yac <lentente1958@gmail.com>
2023-11-20 22:10:31 +0100

    define index page design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+import Link from 'next/link'
 import './page.css'
 
 export default function Home() {
   return (
     
-      <div className="pg-1">hello world dddddd</div>
+      <div className="pg-1">
+        <h2>Hi,Dear</h2>
+        <h1>Enjoy shopin on Stop & Shop</h1>
+        <section className='domain'>
+            <span><Link  href="">All</Link></span>
+            <span><Link  href="">Shows & clothes</Link></span>
+            <span><Link  href="">Pc & phones</Link></span>
+            <span><Link  href="">Beauty</Link></span>
+        </section>
+      </div>
    
   )
 }
