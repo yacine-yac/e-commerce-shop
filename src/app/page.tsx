0d37b6fbdd5b7d06bcc1f1c151692e3c248306da
yacine-yac <lentente1958@gmail.com>
2023-11-21 23:15:52 +0100

    build page 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { ProductShow } from '@/templates/productShow'
 import Link from 'next/link'
 import './page.css'
 
@@ -8,10 +9,24 @@ export default function Home() {
         <h2>Hi,Dear</h2>
         <h1>Enjoy shopin on Stop & Shop</h1>
         <section className='domain'>
-            <span><Link  href="">All</Link></span>
+            <span className='btn-background-active'><Link  href="">All</Link></span>
             <span><Link  href="">Shows & clothes</Link></span>
             <span><Link  href="">Pc & phones</Link></span>
             <span><Link  href="">Beauty</Link></span>
+            <span><Link  href="">Food</Link></span>
+            <span><Link  href="">Energy</Link></span>
+        </section>
+        <section className='slider'></section> 
+        <section className="show">
+           <h1>Recommended for you</h1>
+           <div>
+            <ProductShow />
+            <ProductShow />
+            <ProductShow />
+            <ProductShow />
+            <ProductShow />
+            <ProductShow />
+           </div>
         </section>
       </div>
    
