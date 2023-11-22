ede7f87cde3597f055f490c845e2b50f0cfd3789
yacine-yac <lentente1958@gmail.com>
2023-11-22 23:50:05 +0100

    productShow changes in style and component

--- src/templates/productShow.tsx
+++ src/templates/productShow.tsx
@@ -1,9 +1,18 @@
+import Link from "next/link";
 import "./style/productShow.css";
+import Image from "next/image";
 
 export function ProductShow(){
     return <>
         <div className="show-p" >
-
+            <Link href=""> 
+                   <img  alt="pro" src="/shoes.jpg" />
+                   <p>Shoes nike super  good 39-44</p> 
+                  <h2>150 DZD</h2> 
+                  <span id="second">220 DZD</span>
+            </Link>
+         
+            
         </div>
     </>
 }
