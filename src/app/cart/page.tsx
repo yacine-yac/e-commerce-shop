45af24c61560a1dfab23f18970ef882d7fe12cf5
yacine-yac <lentente1958@gmail.com>
2023-11-30 22:51:01 +0100

    creating cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,6 +1,31 @@
-
+import "./page.css";
 export default function Cart(){
     return <>
-    <p>Cart page</p>
+           <div className="pg pg-3">
+                <div>
+                     <section className="center">
+                        <span className='center'>
+                            <button className="btn-active-ground" type='button'>+</button>
+                            <h2 className='center'>6</h2>
+                            <button className='btn-des'>-</button>
+                        </span>
+                        <img src="/shoes.png" />
+                     </section>
+                     <section>
+                        <h1 className="center">Nike shoes 2023 super good</h1>
+                     </section>
+                </div>
+                <div className="s-1 p-bottom">
+                    <section>
+                        <h3>Total price</h3>
+                        <h1>3000 <sub>Dzd</sub></h1>
+                    </section>
+                    <section>
+                        <h3>Items</h3>
+                        <h1>12</h1>
+                    </section>
+                    <button type="button">Confirmed order</button>
+                </div>
+           </div>
     </>
 }
