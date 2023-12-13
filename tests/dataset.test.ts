2dc257a49bdb69ca0a47e39be88a9768021ce62c
yacine-yac <lentente1958@gmail.com>
2023-12-13 22:48:09 +0100

    changes on tests

--- tests/dataset.test.ts
+++ tests/dataset.test.ts
@@ -1,4 +1,3 @@
-import { Models } from "mongoose";
 import DataSet from "../lib/db" 
 import order from "../lib/db/schema/orders";
 import stock from "../lib/db/schema/products/stock";
@@ -9,118 +8,109 @@ import { Iproduct } from "../lib/models/products";
 import { Istock } from "../lib/models/stock";
 import { Ids } from "../lib/db/configurations/types";
 import GlobalState from "../lib/db/schema/orders/globalState";
+import  domain from "./data/domain.json";
+import group from "./data/group.json"
+import product from "./data/product.json"
+import orderObject from "./data/order.json"
+
+
+describe.only("insert category, domain, group",()=>{
+   let dataSet:Ids;
+
+   beforeAll(async () => {
+     dataSet =await DataSet(); // Provide the actual collection name
+    //  await dataSet.connecting();
+
+   });
  
-   
-// describe("data base operations",()=>{
-//    let dataSet:{state:boolean};
+   afterAll(async () => {
+     // Close the Mongoose connection after all tests
+     await dataSet.close();
+     await GlobalState.close();
+   });
 
-//    beforeAll(async () => {
-//      dataSet = DataSet(); // Provide the actual collection name
-//     //  await dataSet.connecting();
-
-//    });
- 
-//    afterAll(async () => {
-//      // Close the Mongoose connection after all tests
-//      await dataSet.db.connection.close();
-//    });
-
-//       test("connect",async()=>{  
-//             expect(dataSet.state).toEqual(true);
-//       });
-//       // test('find',async()=>{
-//       //       dataSet.collection='clients';
-//       //     const insert=await dataSet.find();
-//       //     const result=[{"__v": 0, "_id": "6537f291f11b5408f0d53f6b", "contact": {"_id": "6537f291f11b5408f0d53f6c", "email": "rrffdfsd", "phone": "555555", "phone2": "68965"}, "firstName": "yacine", "id": 5, "lastName": "yacx", "order": [2]}];
-//       //         expect(insert).toContain([{"__v": 0, "_id": "6537f291f11b5408f0d53f6b", "contact": {"_id": "6537f291f11b5408f0d53f6c", "email": "rrffdfsd", "phone": "555555", "phone2": "68965"}, "firstName": "yacine", "id": 5, "lastName": "yacx", "order": [2]}]);
-//       // },20000 );
-
-//       // test("client inserte", ()=>{
-//       //       dataSet.collection='clients';
-//       //       const data={};
-//       //       const state=dataSet.insert(data);
-//       //       expect(state).toEqual(false);
-//       // })
+/** ============================================== tests ====================================================== */
+  // test("connect",async()=>{  
+  //       expect(dataSet.state).toEqual(true);
+  // });
+  
+  test("domain",async()=>{
+         const {domains}= dataSet.models;
+         const m= await domains.insertMany(domain); 
+         expect((await m).length).toBe(3);
+  });
+  test.only("group",async()=>{
+    const {groups}= dataSet.models;
+    const m= await groups.insertMany(group); 
+    expect((await m).length).toBe(3);
+  });   
         
-        
-// });
-// describe("collections testing",()=>{
-//     let dataSet:{};
-//     const product={
-//                 codeBar:"456",
-//                 name:"ecouteur",
-//                     description:"use this product ....", 
-//                     catalog:{main:"mak.jpg",videos:[],pictures:[]},
-//                     quantity:{value:10,oldest:null},
-//                 price:{current:120},
-//                 category:[5],
-//                 domain:[5,6,9]
-//     };
-//    beforeAll(async () => {
-//      dataSet = DataSet(); 
-//       // await dataSet.connecting();
+});
+describe("collections testing",()=>{
+   let dataSet:Ids; 
+   beforeAll(async () => {
+     dataSet =await DataSet(); 
+      // await dataSet.connecting();
 
-//    });
+   });
  
-//    afterAll(async () => {
-//      // Close the Mongoose connection after all tests
-//      await dataSet.db.connection.close();
-//    });
+   afterAll(async () => {
+     // Close the Mongoose connection after all tests
+      await dataSet.close();
+   });
 
     
 
-//    it.only("products insert",async()=>{   
-//       const rr= await dataSet.insert(product,"products"); 
-//       console.log("ddddd",rr);
-//       expect(rr).toBe(true);
-//    });
+   it.only("products insert",async()=>{  
+          const {products}= dataSet.models
+          const rr=new products(product); 
+          console.log("ddddd",rr);
+          expect(rr).toBe(true);
+   });
          
-//    it("domain add",async()=>{
-//     dataSet.collection="domains";
-//          const data={id:165,name:"telecom",product:[5,99]};
-//          const result=await dataSet.insert(data);
-//          expect(result).toBe(true);
-//    });
-//    it("stock add ",async()=>{
-//         dataSet.collection="products";
-//         const product=await dataSet.find({codeBar:'456'});
-//         if(!product)  console.log("echou"); 
-//         console.log("suuccess");
-//         const newStock:Istock={
-//             value:4+ product.quantity.value,
-//             oldest:product.quantity
-//         };
-//         product.quantity=newStock;
-//         product.save();
-//         expect(product.name).toBe("ecouteur");
-//    });
-//    it('select stock',async()=>{
-//          dataSet.collection="products";
-//         const product=await dataSet.find({codeBar:'456'});
-//         console.log(product.quantity,"mmmmmmmmmm");
-//         expect(product.quantity.value).toBe(124);
-//    });
-//    it.only("insert price",async()=>{
-//           dataSet.collection="products";
-//           const product=await dataSet.find({codeBar:"456"});
-//           const newPrice:Iprice={
-//             current:80,
-//             oldest:product.price
-//           } 
-//           product.price=newPrice;
-//           product.save()
-//           expect(product.price.current).toBe(80)
+   it("domain add",async()=>{
+         const data={id:165,name:"telecom",product:[5,99]};
+         const {domains}=dataSet.models;
+         const result=new domains(data);
+         expect(result).toBe(true);
+   });
+   it("stock add ",async()=>{
 
-//    });
-//    it.only('find price',async()=>{
-//        dataSet.collection="products";
-//        const product=await dataSet.find({codeBar:"456"});
-//        console.log(product.price);
-//        expect(product.price.current).toBe(80);
-//    })
-// })
+        const product=await dataSet.models['products'].findOne({codeBar:'456'});
+        if(!product)  console.log("echou"); 
+        console.log("suuccess");
+        const newStock:Istock={
+            value:4+ product.quantity.value,
+            oldest:product.quantity
+        };
+        product.quantity=newStock;
+        product.save();
+        expect(product.name).toBe("ecouteur");
+   });
+   it('select stock',async()=>{
+        const product=await dataSet.models['products'].findOne({codeBar:'456'});
+        console.log(product.quantity,"mmmmmmmmmm");
+        expect(product.quantity.value).toBe(124);
+   });
+   it.only("insert price",async()=>{
+          const product=await dataSet.models['products'].findOne({codeBar:"456"});
+          const newPrice:Iprice={
+            current:80,
+            oldest:product.price
+          } 
+          product.price=newPrice;
+          product.save()
+          expect(product.price.current).toBe(80)
 
-describe.only("orders operations",()=>{
+   });
+   it.only('find price',async()=>{
+       const product=await dataSet.models['products'].findOne({codeBar:"456"});
+       console.log(product.price);
+       expect(product.price.current).toBe(80);
+   })
+})
+
+describe("orders operations",()=>{
   let dataSet:Ids;
 
   beforeAll( async ()=>{ 
@@ -174,16 +164,17 @@ describe.only("orders operations",()=>{
    
   test('insert order',async()=>{
        
-          const orderObject= { 
-                orderNumber: await GlobalState.generate(),
-                client:5,
-                products:[{quantity:2,product:456}],
-                total:200,
-                deliveryPrice:15,
-                state:{
-                  current:"order"  
-                }
-          }; 
+          // const orderObject= { 
+          //       orderNumber: await GlobalState.generate(),
+          //       client:5,
+          //       products:[{quantity:2,product:456}],
+          //       total:200,
+          //       deliveryPrice:15,
+          //       state:{
+          //         current:"order"  
+          //       }
+          // }; 
+          orderObject.orderNumber=await GlobalState.generate();
           const {orders}=dataSet.models;
           const order=new orders(orderObject);   
           const orderInsert= await  order.save();
