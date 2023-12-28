83a63f5da0542ddeb177b5170501114fcf74a877
yacine-yac <lentente1958@gmail.com>
2023-12-28 23:30:21 +0100

    build cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,60 +1,85 @@
+"use client"
+import { CartProduct } from "@/model/cart/cartProduct";
 import "./page.css";
-export default function Cart(){
-    return <>
-           <div className="pg pg-3">
+import {  useCart } from "@/hooks/cart"; 
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react"; 
+import { Iprice } from "../../../lib/models/prices";
+import { Icatalog } from "../../../lib/models/catalog";
+import { Icategory } from "../../../lib/models/category";
+
+type TproductCart={
+    codeBar: string;
+    name: string;
+    selectedCategory: Icategory;
+    quantity: number;
+    price: Iprice;
+    catalog:Icatalog;
+};let j=0;
+export default function CartPage(){j++;
+    const [selectedProduct,setSelectedProduct]=useState(0);
+    const params:TproductCart=JSON.parse(useSearchParams().get('product') as string); 
+    const {cart,items,total,isEmpty,isLoading,setCart,exists,add}=useCart(); 
+    const increseQuantity=()=>{
+        const m= items[selectedProduct].quantity=items[selectedProduct].quantity+1;
+        setCart({...cart,items:items});
+    }
+    const decreseQuantity=()=>{
+        if(items[selectedProduct].quantity>1){
+            items[selectedProduct].quantity=items[selectedProduct].quantity-1;
+            setCart({...cart,items})
+        }
+    } 
+
+    useEffect(()=>{
+                if(params && !exists(params.codeBar)){
+                    const {codeBar,name,selectedCategory,quantity,price,catalog}=params;
+                    const product=new CartProduct(codeBar,name,selectedCategory,quantity,price,catalog)
+                    add(product);
+                }
+    },[]); 
+    
+        
+    return  isLoading ? 
+        <h1>Loading</h1>
+        :
+        isEmpty ? 
+        <h1>Cart is empty</h1>
+        :
+        <div className="pg pg-3">
                 <div>
                      <section className="center">
                         <span className='center'>
-                            <button className="btn-active-ground" type='button'>+</button>
-                            <h2 className='center'>6</h2>
-                            <button className='btn-des'>-</button>
+                            <button onClick={increseQuantity} className="btn-active-ground" type='button'>+</button>
+                            <h2 className='center'>{items[selectedProduct].quantity}</h2>
+                            <button onClick={decreseQuantity} className='btn-des'>-</button>
                         </span>
-                        <img src="/shoes.png" />
+                        <img src={items[selectedProduct]?.catalog.main} />
                      </section>
                      <section  className="center-v">
-                        <h1>Nike shoes 2023 super good</h1>
+                        <h1>{items[selectedProduct]?.name}</h1>
                         <div className="scrolling">
-                            <div className="center btn-active-ground">
-                                <img src="/shoes.png" />
-                                <h2>1500 <sub>Dzd</sub></h2>
-                                <p>2L</p>
-                            </div>
-                            <div className="center">
-                                <img src="/shoes.png" />
-                                <h2>1500 <sub>Dzd</sub></h2>
-                                <p>2L</p>
-                            </div>
-                            <div className="center">
-                                <img src="/shoes.png" />
-                                <h2>1500 <sub>Dzd</sub></h2>
-                                <p>2L</p>
-                            </div>
-                            <div className="center">
-                                <img src="/shoes.png" />
-                                <h2>1500 <sub>Dzd</sub></h2>
-                                <p>2L</p>
-                            </div>
-                            <div className="center">
-                                <img src="/shoes.png" />
-                                <h2>1500 <sub>Dzd</sub></h2>
-                                <p>2L</p>
-                            </div>
-                            
+                            {items.map((x,y)=><div onClick={()=>setSelectedProduct(y)} key={y}  className={`center ${y==selectedProduct && "btn-active-ground" }`}>
+                                                        <img src={x.catalog.main} />
+                                                        <h2>{x.price.current} <sub>Dzd</sub></h2>
+                                                        <p>{x.category.name}</p>
+                                         </div>
+                            )}   
                         </div>
                      </section>
                 </div>
                 <div className="s-1 p-bottom">
                     <section>
                         <h3>Total price</h3>
-                        <h1>3000 <sub>Dzd</sub></h1>
+                        <h1>{total()} <sub>Dzd</sub></h1>
                     </section>
                     <section>
                         <h3>Items</h3>
-                        <h1>12</h1>
+                        <h1>{items.reduce((prev,current)=>prev+current.quantity,0)}</h1>
                     </section>
                     <button type="button">Confirmed order</button>
-                    <span>Included delivery price 650 Dzd</span>
+                    <span>Included delivery price {cart.delivery} Dzd</span>
                 </div>
-           </div>
-    </>
+           </div>  
+     
 }
