391136a5c6f97c1587b384bde1d8dd13d4e31d27
yacine-yac <lentente1958@gmail.com>
2023-12-31 23:46:29 +0100

    add features into confirm page

--- src/app/cart/confirm/page.tsx
+++ src/app/cart/confirm/page.tsx
@@ -1,47 +1,69 @@
+"use client";
 import "./page.css";
 import "../page.css"
+import { useForm } from "react-hook-form";
+import { Iclient } from "../../../../lib/models/clients"; 
+import axios from "axios";
 export default function Confirm(){
-
+    const {register,watch,handleSubmit}=useForm<Iclient>();
+    console.log(watch("firstName"));
+    console.log(watch("contact.email"));
+    console.log(watch("contact.address"));
+    console.log(watch("contact.address.street"));
+    const submiting=async(data:Iclient)=>{
+            try{
+                    await  axios.post('/api/cart',JSON.stringify(data));
+            }catch(e){
+                
+            }
+    }
     return <>
          <div className="pg pg-6 center-h">
             <div className="pg-form">
                  <h2>fill the application</h2>
-                 <form>
-                    <label form="name">
-                       <input id="name" type="text" name="name" placeholder="Name" />
-                    </label>  
-                    <span>error message</span>
-                    <label>
-                            <select id="operator" name="operator">
-                                <option value="Mobilis">Mobilis</option>
-                                <option value="Djezzy">Djezzy</option>
-                                <option value="Ooredoo">Ooredoo</option>
-                            </select>
-                            <input type="text" name="phone" placeholder="Phone"  />
+                 <form id="contact-form" onSubmit={handleSubmit(submiting)}>
+                    <label form="firstName">
+                       <input {...register("firstName")}  id="firstName" type="text" name="firstName" placeholder="First name" /> 
+                    </label> 
+                    <label form="lastName">
+                       <input {...register("lastName")} id="lastName" type="text" name="lastName" placeholder="Last name" />
                     </label>
                     <span>error message</span>
+                    <label> 
+                            <input {...register("contact.phone")} type="text" name="phone" placeholder="Phone"  />
+                    </label>
+                    <label>
+                            <input type="text" name="phone2" placeholder="Confirm phone"  />
+                    </label>
+                    <span>error message</span>
+                    <label><input {...register('contact.email')} type="email"   placeholder="E-mail" /></label>
+                    <span>error message</span>
                     <label className="center-h" form="address">
-                            <select name="province">
+                            <select {...register('contact.address.province')}>
                                 <option value="province">Wilaya</option>
+                                <option value="1">adrar</option>
+                                <option value="2">alger</option>
+                                <option value="3">oran</option>
+                                <option value="4">sba</option>
                             </select>
-                            <select name="city">
-                                <option value="city">Commune</option>
+                            <select {...register("contact.address.city")}>
+                                <option value="-1">Commune</option>
+                                <option value="1">ddddd</option>
+                                <option value="2">cccc</option>
+                                <option value="3">aaaaa</option>
+                                <option value="4">qqqqq</option>
                             </select>
-                            <input type="text" name="address" id="address" placeholder="Adress" />
+                            <input  {...register("contact.address.street")} type="text" id="address" placeholder="Adress" />
                     </label> 
                     <span>error message</span>
-                    <label>
-                            <input type="text" name="phone2" placeholder="Phone (2)"  />
-                   </label>
-                   <span>error message</span>
-                   <label><input type="email" name="email" placeholder="E-mail" /></label>
-                   <span>error message</span>
+
+
 
                  </form>
             </div>
             <div className="s-1 p-bottom s-1-two">
                 <span>Error message</span>
-                <button>finish</button>
+                <button type="submit" form="contact-form" >finish</button>
             </div>
          </div>
     </>
