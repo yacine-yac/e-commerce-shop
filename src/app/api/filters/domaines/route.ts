5a287760eb69bd6ee5472974c755e0dbd4db244a
yacine-yac <lentente1958@gmail.com>
2024-01-01 22:11:51 +0100

    changes on domaines filters

--- src/app/api/filters/domaines/route.ts
+++ src/app/api/filters/domaines/route.ts
@@ -2,6 +2,6 @@ import { NextResponse } from "next/server";
 import { Domains } from "../../../../../lib/db/schema";
 
 export  async function GET(){
-     const domaines=await Domains.find();
-    return NextResponse.json(domaines ?? {errorMessage: "request has been failer "})
+     const domaines=await Domains.find(); 
+    return NextResponse.json({values:domaines}  ?? {errorMessage: "request has been failer "},{status:domaines.length>0 ? 200:404})
 }
