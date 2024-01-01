a86f1096df016230bf6bbbb20f206a1c36a71e2c
yacine-yac <lentente1958@gmail.com>
2024-01-01 22:13:06 +0100

    changes on products endpoint

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,11 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
-import { Products } from "../../../../lib/db/schema";
+import { Domains, Products } from "../../../../lib/db/schema";
 const length=3;
 export async function GET(NextRequest:NextRequest){
     const url=new URL(NextRequest.url);
     const page:number=+(url.searchParams.get('p') ?? 1);
+    const domaine=url.searchParams.get('domaine');
     const skip=(page-1)*length;
-    const products= await Products.find().skip(skip).limit(length);  
+    let products;
+    if(domaine){
+        const productsByDomaine= await Domains.findOne({name:domaine},{products:true}).populate("products");
+        products= productsByDomaine.products.slice(skip,skip+length);
+    }else{
+        products= await Products.find().skip(skip).limit(length);  
+    }
     const result= products ?? {errorMessage:"products not found"} 
-   return NextResponse.json(result);
+    const status= products ? 200 : 404;
+    return NextResponse.json(result,{status});
 }
