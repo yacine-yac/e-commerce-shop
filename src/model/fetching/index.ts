8265bf4dc0ff6e32891316344bad0b667071dad5
yacine-yac <lentente1958@gmail.com>
2024-01-04 12:39:07 +0100

    add params into query fetch

--- src/model/fetching/index.ts
+++ src/model/fetching/index.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosResponse } from "axios";
 
-export function fetching(url:string):Promise<AxiosResponse<[], any>>{
-   return axios.get(url);
+export function fetching(url:string,params:{}={}):Promise<AxiosResponse<[], any>>{
+   return axios.get(url,{params});
 }
