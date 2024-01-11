169d665121d84a9e0fe3294b523cc5ecc7b8a6ee
yacine-yac <lentente1958@gmail.com>
2024-01-11 12:23:43 +0100

    changes on cart model

--- src/hooks/cart/index.ts
+++ src/hooks/cart/index.ts
@@ -1,8 +1,8 @@
-import { CartProduct } from "@/model/cart/cartProduct"; 
+import { CartProduct } from "@/model/cart/cartProduct";  
 import { useEffect, useState } from "react"; 
 
-type Tcart={items:CartProduct[],delivery:number};
-const initCart:Tcart={items:[],delivery:50}
+type Tcart={items:CartProduct[]};
+const initCart:Tcart={items:[]}
 export function  useCart(){
     const [cart,setCart]=useState<Tcart>(initCart);
     const [state,setSetate]=useState({isLoading:true,isEmpty:true});
@@ -12,8 +12,7 @@ export function  useCart(){
           //  change cart items state without re-render component
               cart.items=storageCart;
         }
-    }
-    
+    } 
     const add=(product:CartProduct)=>{
       const newItems=[...cart.items,product];
           setSetate({...state,isEmpty:false});
@@ -23,7 +22,7 @@ export function  useCart(){
           const filter=cart.items?.filter(x=>x.codeBar== codeBar);
           return (filter.length==0 || cart.items.length==0) ? false:true;
     }
-    useEffect(()=>{
+    useEffect(()=>{ 
       typeof window !== 'undefined' &&  localStorage.setItem("cart",JSON.stringify(cart.items)); 
       setSetate({...state,isLoading:false,isEmpty:cart.items.length >0? false: true});
        
@@ -31,8 +30,8 @@ export function  useCart(){
        return { 
          cart,
          ...state,
-         items:cart.items,
-         total:()=>cart.items.reduce((prev,current)=>prev+(current.quantity*current.price.current),cart.delivery)
+         items:cart.items, 
+         total:()=>cart.items.reduce((prev,current)=>prev+(current.quantity*current.price.current),0)
         ,setCart,add,exists};
      ;
 
