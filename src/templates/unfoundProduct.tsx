6c9469036653976594c20836ee0f9e2bc0c2d448
yacine-yac <lentente1958@gmail.com>
2024-01-19 23:28:36 +0100

    define unfoundPage

---
+++ src/templates/unfoundProduct.tsx
@@ -0,0 +1,9 @@
+import "./style/Loading.css"
+export default function UnfoundProduct(){
+    return <>
+                <div className="pg-pre center">
+                   <img src="/illustration/unfoundProduct.svg" />
+                   <h2>this product not found</h2>
+        </div>
+    </>
+}
