91fe0efa823e33e7f595a1236df208a089c7bda4
yacine-yac <lentente1958@gmail.com>
2024-01-19 22:08:24 +0100

    create layout for this endpoint

---
+++ src/app/contact/layout.tsx
@@ -0,0 +1,11 @@
+"use client";
+import FooterNavbar from "@/templates/footerNavbar";
+import HeaderSection from "@/templates/hearderSection";
+
+export default function({children}:{children:React.ReactNode}){
+    return <>
+         <HeaderSection title="Contact" />
+         {children}
+         <FooterNavbar />
+    </>
+}
