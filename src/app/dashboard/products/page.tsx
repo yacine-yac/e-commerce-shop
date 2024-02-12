5beaa7e162b2ef6749954343a0bd4e50831eb83b
yacine-yac <lentente1958@gmail.com>
2024-02-12 21:00:53 +0100

    define product dashboard

---
+++ src/app/dashboard/products/page.tsx
@@ -0,0 +1,11 @@
+import './page.css';
+export default function(){
+    return  <>
+       <div className="pro">
+           <div className="pro-1">
+                
+           </div>
+           <div className="pro-2"></div>
+       </div>
+    </>
+}
