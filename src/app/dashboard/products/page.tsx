6f84948860579f47da297f753dc00ce02464ccd0
yacine-yac <lentente1958@gmail.com>
2024-02-13 22:57:35 +0100

    products dashboard changes

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -3,9 +3,28 @@ export default function(){
     return  <>
        <div className="pro">
            <div className="pro-1">
-                
+               
+             
+                <h1>2000<p>products</p></h1>
+                <img src="/illustration/product.svg" />
+                <section>
+                        <h1>+100<p>categories</p></h1>
+                        <h1>+131<p>domaines</p></h1>
+                        <h1>+120<p>availibale</p></h1> 
+                </section>
+                <div>
+                    <h2>Add new product</h2> 
+                    <button>
+                    <svg width="64px" height="64px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#fff" stroke="#fff"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <title></title> <g id="Complete"> <g data-name="add" id="add-2"> <g> <line fill="none" stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="12" x2="12" y1="19" y2="5"></line> <line fill="none" stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="5" x2="19" y1="12" y2="12"></line> </g> </g> </g> </g></svg>
+                    </button>
+                    <p>create a new product by defining its properties, and avoid duplication</p>
+                   
+                 
+                </div>
            </div>
-           <div className="pro-2"></div>
+           <div className="pro-2">
+            
+           </div>
        </div>
     </>
 }
